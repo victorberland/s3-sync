e4851db348fe9a7c59468ca9260434105c823378
Victor Berland <vb@victorberland.com>
2021-07-09 14:20:00 +0200

    sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 mc alias set minio1 $ONE_HOST $ONE_ACCESS_KEY $ONE_SECRET_KEY
 mc alias set minio2 $TWO_HOST $TWO_ACCESS_KEY $TWO_SECRET_KEY
 
-mc mirror --watch minio1/$TWO_BUCKET minio2/$TWO_BUCKET &
+while true; do
+	mc mirror minio1/$TWO_BUCKET minio2/$TWO_BUCKET &
 
-if [ $TWOWAY = true ]; then
-	mc mirror --watch minio2/$ONE_BUCKET minio1/$ONE_BUCKET &
-fi
+	if [ $TWOWAY = true ]; then
+		mc mirror minio2/$ONE_BUCKET minio1/$ONE_BUCKET &
+	fi
 
-sleep infinity
+	sleep 0
+done
